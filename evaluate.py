8041af5e16974807442485ecfc76066ceab0da01
HQHBench <170966508+HQHBench@users.noreply.github.com>
2024-07-22 16:55:11 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,30 +1,43 @@
 import argparse
 import json
+import re
 import time
 
 from tqdm import tqdm
 from utils import *
 import openai
+import os
 
 
 def evaluate(args):
     ans_file = args.ans_file
     openai_key = args.openai_key
     openai.api_key = openai_key
-    hal_rates = {}
+    num = args.num
+    all_hal_results = {f'Model{i+1}': hal_results for i in range(num)}
+    all_hal_rates = {f'Model{i+1}': {} for i in range(num)}
+
     with open(ans_file, 'r', encoding='utf-8') as f:
         model_answers = json.load(f)
     with open('./image_data.json', 'r', encoding='utf-8') as f:
         image_datas = json.load(f)
+
     for i, answer in tqdm(enumerate(model_answers)):
         image_id = answer['image_id']
         hal_type = answer['type']
         image_data = image_datas[str(image_id)]
+        answers_format = ''
+        output_format = ''
+        for j, text in enumerate(answer['answers']):
+            answers_format += f'Answer{j+1}: {text}\n'
+            output_format += f'Answer{j+1}: With/Without hallucination, [evidence]\n'
 
         if hal_type == 'existence':
-            prompt = template4existence.format(answer['instruction'], image_data['caption'], image_data['image_information'], answer['answer'])
+            prompt = template4existence.format(answer['instruction'], image_data['caption'],
+                                               image_data['image_information'], answers_format, output_format)
         else:
-            prompt = template.format(answer['instruction'], answer['ground_truth'], image_data['caption'], image_data['image_information'], answer['answer'])
+            prompt = template.format(answer['instruction'], answer['ground_truth'], image_data['caption'],
+                                     image_data['image_information'], answers_format, output_format)
 
         gpt_output = None
         while gpt_output is None:
@@ -37,27 +50,39 @@ def evaluate(args):
                     ]
                 )
                 gpt_output = response.choices[0].message.content
+
             except Exception as e:
                 print(e)
                 time.sleep(10)
+        print(prompt)
+        print(gpt_output)
+        lines = gpt_output.split("\n")
+        pattern = re.compile(r"Answer(\d+)")
 
-        if 'With hallucination' in gpt_output:
-            hal_results[hal_type].append(1)
-            hal_results['overall'].append(1)
-        elif 'Without hallucination' in gpt_output:
-            hal_results[hal_type].append(0)
-            hal_results['overall'].append(0)
+        for line in lines:
+            match = pattern.search(line)
+            if not match:
+                continue
+            answer_id = match.group(1)
+            if 'With hallucination' in line:
+                all_hal_results[f'Model{answer_id}'][hal_type].append(1)
+                all_hal_results[f'Model{answer_id}']['overall'].append(1)
+            elif 'Without hallucination' in line:
+                all_hal_results[f'Model{answer_id}'][hal_type].append(0)
+                all_hal_results[f'Model{answer_id}']['overall'].append(0)
 
     for hal_type in hal_results.keys():
-        hal_rates[hal_type] = sum(hal_results[hal_type]) / len(hal_results[hal_type])
+        for i in range(num):
+            all_hal_rates[f'Model{i+1}'][hal_type] = sum(all_hal_results[f'Model{i+1}'][hal_type]) / len(all_hal_results[f'Model{i+1}'][hal_type])
 
-    return hal_rates
+    return all_hal_rates
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--ans_file", type=str, default='path/to/your/answer/file')
     parser.add_argument("--openai_key", type=str, default='your/openai/api/key')
+    parser.add_argument("--num", type=int, default=1)
     args = parser.parse_args()
     results = evaluate(args)
     print(results)
