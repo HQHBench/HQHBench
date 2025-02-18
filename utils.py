4253def263c2e76e647c49c897520f5c4719045e
HQHBench <170966508+HQHBench@users.noreply.github.com>
2025-02-18 17:21:07 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,38 +1,32 @@
-template = """Given the input instruction, ground truth answer and detailed image information, please determine whether the response provided by a Large Vision-Language Model (LVLM) contains any hallucination. Hallucination here refers to the situation that the generated response is inconsistent with the input image. 
-  
-Please note that the ground truth answer and image information only contain factual information and may not be completely comprehensive in describing all the objects and their attributes. Detailed ana-lysis or reasoning in LVLM response should be encouraged and not considered as hallucination.
+template = """###Task:
+Given the input instruction, the ground truth answer, and detailed image information, evaluate whether the LMM response is aligned with the image content.
 
-To evaluate the LVLM responses, you need to provide brief evidence to support your judgment. 
+###Evaluation Criteria:
+Answer Correctness: Assess whether the model's response accurately matches the ground truth answer in terms of semantic consistency.
 
-###Evaluation criteria:
--Without hallucination: The LVLM response is semantically similar to the ground truth answer and does not contain any contradictory factual claim with the provided information.
--With hallucination: The LVLM response is completely different from the ground truth answer, or contains a contradictory factual claim about an object, action, attribute, or any other detail that is not grounded in the provided information.
+Extra Contradictory Claims: Examine whether the response includes additional factual claims beyond the answer to the question that contradict the image information (e.g., objects, actions, attributes). If such contradictions are present, quantify their occurrence and provide evidence.
 
-###Instruction:\n{}\n
-###Ground Truth:\n{}\n
-###Image Caption:\n{}\n
-###Image Details:\n{}\n
-###Model Response:\n{}
-###Output Format:\n{}\n
-"""
+Scope of Evaluation: Focus only on explicit factual claims within the model's response. Analytical reasoning or the model’s process of deriving an answer should be excluded from contradiction assessments. Incorrect answers should not be counted as part of the additional contradictory claims.
 
-template4existence = """Given the input instruction and detailed image information, please determine whether the response provided by a Large Vision-Language Model (LVLM) contains any hallucination. Hallucination here refers to the situation that the generated response is inconsistent with the input image. 
+For each response, provide a judgment supported by evidence:
+Answer: Correct / Incorrect
+Extra Contradictory Claims: [Number]，[Evidence1], [Evidence2], ...
 
-Please note that the image information only contain factual information and may not be completely comprehensive in describing all the objects and their attributes. Detailed analysis or reasoning in LVLM response should be encouraged and not considered as hallucination.
+###Instruction:
+{}
 
-To evaluate the LVLM responses, you need to provide brief evidence to support your judgment. 
+###Ground Truth Answer:
+{}
 
-###Evaluation criteria:
--Without hallucination: The LVLM response does not contain any contradictory factual claim with the provided information.
--With hallucination: The LVLM response contains a contradictory factual claim about an object, action, attribute, or any other detail that is not grounded in the provided information.
+###Image Caption:
+{}
 
-###Instruction:\n{}\n
-###Image Caption:\n{}\n
-###Image Details:\n{}\n
-###Model Response:\n{}
-###Output Format:\n{}\n
-"""
+###Image Details:
+{}
 
+###Model Responses:
+{}
 
-hal_results = {'overall': [], 'action': [], 'attribute': [], 'comparison': [], 'count': [], 'environment': [],
-               'existence': [], 'OCR': [], 'relation': []}
+###Output Format:
+Answer: Correct / Incorrect
+Extra Contradictory Claims: [Number]，[Evidence1], [Evidence2], ..."""
